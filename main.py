01cef39b4455c2aaee6980743e794d90656bf1c7
Spatialflunky1 <mjandrus06@gmail.com>
2021-12-22 15:04:20 -0500

    started work on gui, uploading for access on another device

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from tkinter import filedialog, Tk
+from tkinter import filedialog, Tk, Button
 from PIL import Image, ImageFont, ImageDraw
 import os
 from shutil import rmtree
 
-# Creates dialog for choosing base gif
 def getFilePath():
-    root = Tk()
-    root.withdraw()
     return filedialog.askopenfilename()
 
-# Gets the dimensions of the image if needed to be resized
 width, height = 0, 0
 def getResizeSize(imageToResize):
     global width
@@ -25,7 +21,6 @@ def getResizeSize(imageToResize):
         return [width, height]
     return False
 
-# Calls the draw text function on each frame and saves each as a bitmap image
 def saveToFrame(image):
     global width
     global height
@@ -56,7 +51,6 @@ def saveToFrame(image):
         draw.text((x, y),text_bottom,(255,255,255),font=font)
         frame_object.save("frames/frame" + str(frame) + ".bmp")
 
-# Appends all bitmap images in the frame folder to the all_frames list
 def createGif():
     files = os.listdir("frames/")
     all_frames = []
@@ -66,22 +60,22 @@ def createGif():
             loadedFrame = Image.open(file)
             loadedFrame.resize((width, height))
             all_frames.append(loadedFrame)
-
-    # Saves the frames to a singular gif file
     firstFrame = Image.open("frames/frame0.bmp")
     firstFrame.save('output.gif', save_all=True, append_images=all_frames[1:], loop=0)
     firstFrame.close()
     for file in all_frames:
         file.close()
 
-if __name__ == "__main__":
-    # Cleaning of the frames folder
+def chooseImage():
+    filepath = getFilePath()
+    return filepath
+
+def createGifButton():
     try:
         rmtree("frames/")
     except FileNotFoundError:
         pass
     os.mkdir("frames/")
-    filepath = getFilePath()
     imageObject = Image.open(filepath)
     shadowcolor = (0,0,0)
     fontsize = int(input("Enter Font Size: "))
@@ -89,3 +83,11 @@ if __name__ == "__main__":
     saveToFrame(imageObject)
     createGif()
     rmtree("frames/")
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Gif Text Generator")
+    root.geometry("800x600")
+    selectFile = Button(root, text="Select File", command=lambda:chooseImage())
+    selectFile.pack()
+    root.mainloop()
